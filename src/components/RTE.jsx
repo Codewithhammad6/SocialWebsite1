3d3c93fca1fd4e268d11c3a3d783091b7e6f7f5e
Hammad Choudhary <hs>
2025-05-02 08:21:51 -0700

    Update RTE.jsx

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,37 +1,54 @@
-import React from 'react';
-import { Editor } from '@tinymce/tinymce-react';
-import { Controller } from 'react-hook-form';
+import React from 'react'
+import {Editor } from '@tinymce/tinymce-react';
+import {Controller } from 'react-hook-form';
 
-export default function RTE({ name, control, label, defaultValue = "" }) {
+
+export default function RTE({name, control, label, defaultValue =""}) {
   return (
-    <div className='md:w-full mx-auto w-[18rem]'>
-      {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
-      <Controller
-        name={name || "content"}
-        control={control}
-        render={({ field: { onChange } }) => (
-          <Editor
-            initialValue={defaultValue}
-            apiKey="iivimj0xzyblofenmm9g08s84xvx2cyzs2s2x5hbr8r2sat9" // Free public key
-            init={{
-              height: 500,
-              menubar: true,
-              plugins: [
-                'anchor', 'autolink', 'charmap', 'codesample', 'emoticons',
-                'image', 'link', 'lists', 'media', 'searchreplace',
-                'table', 'visualblocks', 'wordcount', 'code', 'fullscreen', 'preview'
-              ],
-              toolbar:
-                'undo redo | blocks | bold italic underline strikethrough | ' +
-                'link image media table | alignleft aligncenter alignright alignjustify | ' +
-                'bullist numlist outdent indent | emoticons charmap | code preview fullscreen | removeformat',
-              content_style:
-                'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
-            }}
-            onEditorChange={onChange}
-          />
-        )}
-      />
-    </div>
-  );
+    <div className='w-full'> 
+    {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+
+    <Controller
+    name={name || "content"}
+    control={control}
+    render={({field: {onChange}}) => (
+        <Editor
+        initialValue={defaultValue}
+        init={{
+            initialValue: defaultValue,
+            height: 500,
+            menubar: true,
+            plugins: [
+                "image",
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "image",
+                "charmap",
+                "preview",
+                "anchor",
+                "searchreplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insertdatetime",
+                "media",
+                "table",
+                "code",
+                "help",
+                "wordcount",
+                "anchor",
+            ],
+            toolbar:
+            "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+        }}
+        onEditorChange={onChange}
+        />
+    )}
+    />
+
+     </div>
+  )
 }
