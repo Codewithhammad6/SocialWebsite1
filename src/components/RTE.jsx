48654e3f828d8a830ca9af4e8354edaf3ab32958
Hammad Choudhary <hs>
2025-05-02 06:56:48 -0700

    first commit

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -12,7 +12,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
     control={control}
     render={({field: {onChange}}) => (
         <Editor
-        apiKey="no-api-key"
+        apiKey={conf.tinymce}
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
